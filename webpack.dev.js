356eef30bf4023928a934f7d952671b6328c35f5
Shachar Leuchter <shachar.leuchter@jvm.de>
2020-03-03 15:47:01 +0100

    feat(pattern): use css in js if dev only

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge')
 const fs = require('fs')
-
+const webpack = require('webpack')
 let baseConfig
 // check if we have a webpack.config in project root
 try {
@@ -17,6 +17,11 @@ try {
 
 module.exports = merge(baseConfig, {
   mode: 'development',
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'dev' // use 'development' unless process.env.NODE_ENV is defined
+    }),
+  ],
   stats: {
     assets: false,
     cached: false,
